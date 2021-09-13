d429b5add9c294051a8f414deede3c01ed330311
Akhlak Uzzalal <akhlakuzzalal@gmail.com>
2021-09-13 20:05:45 +0600

    first-edit

--- js/app.js
+++ js/app.js
@@ -10,7 +10,9 @@ loadProducts();
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    console.log(product);
+    const image = product.image;
+    const description = product.description;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
@@ -19,9 +21,13 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+      <p class="rating-star">${ratings(product.rating.rate)} (${product.rating.rate})</p>
+      <p class="text-danger">totall ${product.rating.count} people rate this</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="showDetail('${product.title}', '${product.description}', '${product.image}')" id="details-btn" class="btn btn-danger">Details</button></div>
+      
+    
       `;
     document.getElementById("all-products").appendChild(div);
   }
@@ -33,11 +39,12 @@ const addToCart = (id, price) => {
 
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
+  updateTotal();
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -46,7 +53,7 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
@@ -76,5 +83,38 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+
+const ratings = rate => {
+  if (rate <= 1) {
+    return `</i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>`
+  }
+  if (rate <= 2) {
+    return `<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>`
+  }
+  if (rate <= 3) {
+    return `<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>`
+  }
+  if (rate <= 4) {
+    return `<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i>`
+  }
+  if (rate <= 5) {
+    return `<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>`
+  }
+}
+const showDetail = (title, description, img) => {
+  const detailContainer = document.getElementById('detail-container');
+  detailContainer.innerHTML = `<h1>Product Details</h1>`;
+  const div = document.createElement('div');
+  div.innerHTML = `
+          <div class="detail-title">
+          <h3 class="rating-star">${title}</h3>
+          <img class="product-image-detail" src=${img}>
+          </div>
+        
+          <p>${description}</p>
+  `;
+  detailContainer.appendChild(div);
+  return detailContainer;
+}
